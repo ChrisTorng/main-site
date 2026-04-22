06300030942532471ab6d36c13b75ebfd0ede2af
ChrisTorng <ChrisTorng@hotmail.com>
2026-04-22 23:35:08 +0800

    feat: 將 MobileNav 組件包裝在 MobileNavWrapper 中以支援動態加載

---
+++ components/MobileNavWrapper.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+
+const MobileNav = dynamic(() => import('./MobileNav'), {
+  ssr: false,
+})
+
+export default function MobileNavWrapper(props) {
+  return <MobileNav {...props} />
+}
